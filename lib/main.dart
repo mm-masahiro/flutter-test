bf6b90b71e799cb04c16be9afdbcf1b70998fc1e
mm-masahiro <hand5.mm9.mzh@gmail.com>
2020-06-21 19:49:54 +0900

    add list view

--- lib/main.dart
+++ lib/main.dart
@@ -46,10 +46,41 @@ class MyApp extends StatelessWidget {
 // StatefulWidgetクラス、Stateクラス
 // StatefulWidget自体はimmutable
 class RandomWordsState extends State<RandomWords> {
+  // 後で読む
+  // https://dart.dev/guides/language/language-tour
+  // for saving suggested word pairings
+  final _suggestions = <WordPair>[];
+  // for making the font size larger
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
     return Text(wordPair.asPascalCase);
+  }
+
+  // ListViewを構築する
+  _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return _buildRow(_suggestions[index]);
+      });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
 
