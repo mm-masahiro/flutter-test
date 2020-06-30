c619b72c415beb7535a223a8f39b87e31eb0c1d2
mm-masahiro <hand5.mm9.mzh@gmail.com>
2020-06-30 20:28:19 +0900

    add bottom-menu

---
+++ lib/FirstPageComponent/menu.dart
@@ -0,0 +1,67 @@
+// import 'package:flutter/material.dart';
+
+// class Menu extends StatelessWidget {
+//   final Widget menu = Container(
+//     child: BottomNavigationBar(
+//       items: <BottomNavigationBarItem>[
+//         BottomNavigationBarItem(
+//           backgroundColor: Colors.blue,
+//           icon: Icon(Icons.home),
+//           title: Text(
+//             'Home',
+//             style: TextStyle(
+//               color: Colors.black
+//             ),
+//           )
+//         ),
+//         BottomNavigationBarItem(
+//           backgroundColor: Colors.blue,
+//           icon: Icon(Icons.search),
+//           title: Text(
+//             'Search',
+//             style: TextStyle(
+//               color: Colors.black
+//             ),
+//           )
+//         ),
+//         BottomNavigationBarItem(
+//           backgroundColor: Colors.blue,
+//           icon: Icon(Icons.receipt),
+//           title: Text(
+//             'News',
+//             style: TextStyle(
+//               color: Colors.black
+//             ),
+//           )
+//         ),
+//         BottomNavigationBarItem(
+//           backgroundColor: Colors.blue,
+//           icon: Icon(Icons.portrait),
+//           title: Text(
+//             'Asset',
+//             style: TextStyle(
+//               color: Colors.black
+//             ),
+//           )
+//         )
+  //     ]
+  //   ),
+  // );
+
+//   test
+//   final Widget menu = Row(
+//     children: <Widget>[
+//       Container(
+//         width: MediaQuery.of(context).size.width,
+//         child: Icon(Icons.home)
+//       ),
+//     ]
+//   )
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: menu,
+//     );
+//   }
+// }
--- lib/firstpage.dart
+++ lib/firstpage.dart
@@ -2,41 +2,10 @@ import 'package:flutter/material.dart';
 import './FirstPageComponent/assets.dart';
 import './FirstPageComponent/trade.dart';
 import './FirstPageComponent/stock.dart';
-
-// class Menu extends StatelessWidget{
-//   List<BottomNavigationBarItem> myBottomNavBarItems() {
-//     return [
-//       BottomNavigationBarItem(
-//         icon: Icon(Icons.home),
-//         title: const Text('Home'),
-//       ),
-//       BottomNavigationBarItem(
-//         icon: Icon(Icons.search),
-//         title:  const Text('Search')
-//       ),
-//       BottomNavigationBarItem(
-//         icon: Icon(Icons.receipt),
-//         title: const Text('News')
-//       ),
-//       BottomNavigationBarItem(
-//         icon: Icon(Icons.portrait),
-//         title: const Text('Mypage')
-//       ),
-//     ];
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       bottomNavigationBar: BottomNavigationBar(
-//         item: myBottomNavBarItems()
-//       )
-//     );
-//   }
-// }
-
+import './FirstPageComponent/menu.dart';
 
 class FirstPage extends StatelessWidget {
+  // int _currentIndex = 0;
 
   // final Widget menu = Container(
   //   List<BottomNavigationBarItem> myBottomNavBarItems() {
@@ -68,16 +37,50 @@ class FirstPage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Stock Demo Trade app'),
         ),
-        body: Center(
-          child: ListView(
+        body: ListView(
             children: [
               Assets(),
               Trade(),
               Stock()
             ]
-          ),
-         
-        )
+        ),
+        // bottomNavigationBar: Menu(),
+        bottomNavigationBar: Row(
+          children: <Widget>[
+            Container(
+              height: 60,
+              width: MediaQuery.of(context).size.width/4,
+              decoration: BoxDecoration(
+                color: Colors.grey
+              ),
+              child: Icon(Icons.home)
+            ),
+            Container(
+              height: 60,
+              width: MediaQuery.of(context).size.width/4,
+              decoration: BoxDecoration(
+                color: Colors.grey
+              ),
+              child: Icon(Icons.search)
+            ),
+            Container(
+              height: 60,
+              width: MediaQuery.of(context).size.width/4,
+              decoration: BoxDecoration(
+                color: Colors.grey
+              ),
+              child: Icon(Icons.receipt)
+            ),
+            Container(
+              height: 60,
+              width: MediaQuery.of(context).size.width/4,
+              decoration: BoxDecoration(
+                color: Colors.grey
+              ),
+              child: Icon(Icons.portrait)
+            ),
+          ]
+        ),
       );
   }
 }
