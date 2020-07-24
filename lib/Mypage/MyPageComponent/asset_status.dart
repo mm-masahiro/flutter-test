fe9134efa7f68d6fb5c51720cc7321e4fa326bcf
mm-masahiro <hand5.mm9.mzh@gmail.com>
2020-07-24 22:56:10 +0900

    add login function

--- lib/Mypage/MyPageComponent/asset_status.dart
+++ lib/Mypage/MyPageComponent/asset_status.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/card-origin.dart';
+import 'package:my_app/MyPage/MyPageComponent/login.dart';
 
+// class AssetStatus extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+      // child: CardOrigin(
+      //   title: Text(
+      //     '資産詳細',
+      //     style: TextStyle(
+      //       color: Colors.white
+      //     ),
+      //   ),
+      //   icon: Icon(Icons.monetization_on, size: 80,),
+      // ),
+//     );
+//   }
+// }
 
-class AssetStatus extends StatelessWidget {
+class AssetStatus extends StatefulWidget {
+  
+  @override
+  _AssetStatus createState() => _AssetStatus();
+
+}
+
+class _AssetStatus extends State<AssetStatus> {
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return GestureDetector(
+      onTap: (){
+        setState(() {
+          Navigator.push(context, MaterialPageRoute(
+            builder: (context) {
+              return Login();
+            },
+          ));
+        });
+      },
       child: CardOrigin(
         title: Text(
           '資産詳細',
